e1e461b0746de3cbb5421b35e32f8ee274f950a1
Ayse <aysenur_aydin@hotmail.fr>
2022-05-06 12:22:23 +0200

    day2_Task5_get text get attribute

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T5_GetText_GetAttribute.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T5_GetText_GetAttribute.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.day2_locators_getText_getAttribute;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class T5_GetText_GetAttribute {
@@ -18,11 +20,25 @@ public class T5_GetText_GetAttribute {
 
         //3- Verify header text is as expected:
         //  Expected: Registration form
+        WebElement headerText = driver.findElement(By.tagName("h2"));
+        String expectedHeaderText = "Registration form";
+        String actualHeaderText = headerText.getText(); // will return "Registration form" as String
 
+        if (actualHeaderText.equals(expectedHeaderText)){
+            System.out.println("Header text verification PASSED!" );
+        }else{
+            System.out.println("Header text verification FAILED!!!");
+        }
 
         //4- Locate “First name” input box
+        WebElement firstNameInput = driver.findElement(By.name("firstname"));
+
+
         //5- Verify placeholder attribute’s value is as expected:
         //  Expected: first name
+        String expectedPlaceHolder = "first name";
+        String actualPlaceHolder = firstNameInput.getAttribute("placeholder");
+
 
 
     }
