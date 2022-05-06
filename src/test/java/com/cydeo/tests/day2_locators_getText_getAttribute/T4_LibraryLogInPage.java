ff4ed1bbddc996d5992eb913b836fcbcb24c660c
Ayse <aysenur_aydin@hotmail.fr>
2022-05-06 11:35:40 +0200

    day2_Task4_Libraray login page

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T4_LibraryLogInPage.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T4_LibraryLogInPage.java
@@ -24,11 +24,18 @@ public class T4_LibraryLogInPage {
         usernameInput.sendKeys("incorrect@gmail.com");
 
         //4. Enter password: “incorrect password”
+        WebElement passwordInput = driver.findElement(By.id("inputPassword"));
+        passwordInput.sendKeys("incorrect password");
 
 
         //5. Click to sign in button
+        WebElement signInButton = driver.findElement(By.tagName("button"));
+        signInButton.click();
+
         //6. Verify: visually “Sorry, Wrong Email or Password”
         //displayed
+
+
         //PS: Locate username input box using “className” locator
         //Locate password input box using “id” locator
         //Locate Sign in button using “tagName” locator
