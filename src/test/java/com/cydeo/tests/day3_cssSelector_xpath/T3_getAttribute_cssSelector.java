0fe4b4afa54908440b9bf2129cc9a223b24f4bd2
Ayse <aysenur_aydin@hotmail.fr>
2022-05-09 00:38:31 +0200

    day3_T4_cssSelector_getAttribute

--- src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T3_getAttribute_cssSelector.java
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T3_getAttribute_cssSelector.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.day3_cssSelector_xpath;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class T3_getAttribute_cssSelector {
     public static void main(String[] args) {
@@ -12,8 +14,31 @@ public class T3_getAttribute_cssSelector {
         driver.manage().window().maximize();
 
 //2- Go to: https://login1.nextbasecrm.com/
+        driver.get("https://login1.nextbasecrm.com/");
+
 //3- Verify “Log in” button text is as expected:
 //Expected: Log In
+       // WebElement signInButton = driver.findElement(By.className("login-btn"));
+
+        //  tagName[attribute = 'value']
+        //  input[class= 'login-btn']
+      //  WebElement signInButton = driver.findElement(By.cssSelector("input[class= 'login-btn']"));
+      //  WebElement signInButton = driver.findElement(By.cssSelector("input[type= 'submit']"));
+        WebElement signInButton = driver.findElement(By.cssSelector("input[value= 'Log In']"));
+
+        String expectedButtonText = "Log In";
+
+        //getting the value of the attribute "value"
+        String actualButtonText = signInButton.getAttribute("value");
+
+        System.out.println("actualButtonText = " + actualButtonText);
+
+        if (actualButtonText.equals(expectedButtonText)){
+            System.out.println("Log In button text verification is PASSED!");
+        }else{
+            System.out.println("Log In Button text verification is FAILED!!!");
+        }
+
 //
 //
 //
