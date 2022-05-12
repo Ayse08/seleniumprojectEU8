1fb9c339917872eace79daa2903f1ae67a003f2a
Ayse <aysenur_aydin@hotmail.fr>
2022-05-12 19:32:54 +0200

    day5_testNG_Intro

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
@@ -1,21 +1,35 @@
 package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class TestNG_Intro {
 
-    @BeforeMethod
+    @BeforeClass
+    public void setupMethod(){
+        System.out.println("---> BeforeClass is running");
+    }
+
+    @AfterClass
+    public void tearDownClass(){
+        System.out.println("---> AfterClass is running!");
+    }
+
+    @BeforeMethod // even if we write this method only one time it will execute before each test
     public void setUpMethod(){
         System.out.println("----> BeforeMethod is running!");
     }
 
-    @Test // when we add @Test and tap enter it becomes runnable
+    @AfterMethod
+    public void tearDownMethod(){
+        System.out.println("---> AfterMethod is running!");
+    }
+
+    @Test (priority = 1)// when we add @Test and tap enter it becomes runnable
     public void test1(){ // when we write this method only we can not run
         System.out.println("Test 1 is running...");
     }
 
-    @Test
+    @Test (priority = 2) // priority can change the running order, normally it runs in the alphabetic order
     public void test2(){
         System.out.println("Test 2 is running...");
     }
