dead9680822cf412827969b98cdc9d7152313c04
Ayse <aysenur_aydin@hotmail.fr>
2022-05-12 13:38:53 +0200

    day5_T3 radio button continue

---
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T3_RadioButton_continue.java
@@ -0,0 +1,46 @@
+package com.cydeo.tests.day5_testNG_intro_dropdowns;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.concurrent.TimeUnit;
+
+public class T3_RadioButton_continue {
+    public static void main(String[] args) {
+
+        //TC #3: Utility method task for (continuation of Task2)
+        //1. Open Chrome browser
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //2. Go to https://practice.cydeo.com/radio_buttons
+        driver.get("https://practice.cydeo.com/radio_buttons");
+
+        //3. Click to “Hockey” radio button
+        WebElement hockeyButton = driver.findElement(By.xpath("//input[@id='hockey']"));
+        hockeyButton.click();
+
+
+        //4. Verify “Hockey” radio button is selected after clicking.
+        //
+        //USE XPATH LOCATOR FOR ALL WEBELEMENT LOCATORS
+        //
+        //Create a utility method to handle above logic.
+        //Method name: clickAndVerifyRadioButton
+        //Return type: void or boolean
+        //Method args:
+        //1. WebDriver
+        //2. Name attribute as String (for providing which group of radio buttons)
+        //3. Id attribute as String (for providing which radio button to be clicked)
+        //
+        //Method should loop through the given group of radio buttons. When it finds the
+        //matching option, it should click and verify option is Selected.
+        //Print out verification: true
+        //
+        //
+
+    }
+}
