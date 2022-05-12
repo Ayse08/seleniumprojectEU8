d54868febb111279a075997f78fb2c60db3caa15
Ayse <aysenur_aydin@hotmail.fr>
2022-05-12 15:29:21 +0200

    day5_testNG_Intro

---
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
@@ -0,0 +1,12 @@
+package com.cydeo.tests.day5_testNG_intro_dropdowns;
+
+import org.testng.annotations.Test;
+
+public class TestNG_Intro {
+
+    @Test // when we add @Test and tap enter it becomes runnable
+    public void test1(){ // when we write this method only we can not run
+        System.out.println("Test 1 is running...");
+    }
+
+}
