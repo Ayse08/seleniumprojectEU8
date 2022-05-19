712b1fd6124139334e54b8c9f18c169f4661b0a7
Ayse <aysenur_aydin@hotmail.fr>
2022-05-19 10:14:49 +0200

    day6 Task5 State Dropdown

--- src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropdownPractices.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropdownPractices.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.day6_alerts_iframes_windows;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -24,10 +26,15 @@ public class DropdownPractices {
         //2. Go to http://practice.cybertekschool.com/dropdown
         driver.get("http://practice.cybertekschool.com/dropdown");
 
-        //3. Select Illinois
+        //we located the dropdown and it is ready to use
+        Select stateDropdown = new Select(driver.findElement(By.xpath("//select[@id='state']")));
 
+        //3. Select Illinois
+        stateDropdown.selectByVisibleText("Illinois");
 
         //4. Select Virginia
+        stateDropdown.selectByValue("VA");
+
         //5. Select California
         //6. Verify final selected option is California.
         //Use all Select options. (visible text, value, index)
