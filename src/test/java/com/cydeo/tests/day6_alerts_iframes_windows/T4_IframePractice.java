8b2af163344ce29f27454d2a92e8d2fb6959f677
Ayse <aysenur_aydin@hotmail.fr>
2022-05-19 15:41:19 +0200

    day6 T4 iFrame Practice

--- src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T4_IframePractice.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T4_IframePractice.java
@@ -1,8 +1,12 @@
 package com.cydeo.tests.day6_alerts_iframes_windows;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
@@ -24,7 +28,24 @@ public class T4_IframePractice {
     }
 
     //4. Assert: “Your content goes here.” Text is displayed.
+    @Test
+    public void iframe_test(){
+
+        // We need to switch otherwise : "NoSuchElement" exception
+        //We need to switch driver's focus to iframe
+        // option#1- switching to iframe using id attribute value
+        driver.switchTo().frame("mce_0_ifr");
+
+        //Locate the p tag
+        WebElement yourContentGoesHereText = driver.findElement(By.xpath("//p"));
+
+        Assert.assertTrue(yourContentGoesHereText.isDisplayed());
+
+
+    }
+
     //5. Assert: “An iFrame containing the TinyMCE WYSIWYG Editor”
+
 
 
 }
