86e9b946ef171f3dd8254a2534628cdcfcb84262
Ayse <aysenur_aydin@hotmail.fr>
2022-05-19 10:31:13 +0200

    day6 Task6 Date Dropdowns

--- src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropdownPractices.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropdownPractices.java
@@ -4,26 +4,34 @@ import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
 public class DropdownPractices {
 
-    WebDriver driver;
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://practice.cybertekschool.com/dropdown");
+    }
 
     @Test
     public void dropdown_task5(){
 
 
-
         //TC #5: Selecting state from State dropdown and verifying result
         //1. Open Chrome browser
+        //2. Go to http://practice.cybertekschool.com/dropdown
         driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-        //2. Go to http://practice.cybertekschool.com/dropdown
         driver.get("http://practice.cybertekschool.com/dropdown");
 
         //we located the dropdown and it is ready to use
@@ -36,13 +44,20 @@ public class DropdownPractices {
         stateDropdown.selectByValue("VA");
 
         //5. Select California
+        stateDropdown.selectByIndex(5); // in Xpath index number starts from 1
+
         //6. Verify final selected option is California.
+        String expectedOptionText = "California";
+        String actualOptionText = stateDropdown.getFirstSelectedOption().getText();
+       // getFirstSelectedOption returns to web element, so it gives compile error. We add getText() to returns
+        // to a String
+
+        Assert.assertEquals(actualOptionText, expectedOptionText);
+
         //Use all Select options. (visible text, value, index)
         //
 
 
     }
-
-
 
 }
