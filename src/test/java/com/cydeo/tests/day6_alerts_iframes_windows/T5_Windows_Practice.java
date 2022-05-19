ddfb278ede84f97d8d7b918aa3b4cafda753c86b
Ayse <aysenur_aydin@hotmail.fr>
2022-05-19 16:40:13 +0200

    day6 T5 Window Task

--- src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T5_Windows_Practice.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T5_Windows_Practice.java
@@ -1,7 +1,10 @@
 package com.cydeo.tests.day6_alerts_iframes_windows;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -27,13 +30,36 @@ public class T5_Windows_Practice {
 @Test
     public void multiple_window_test(){
 
-}
+        // Storing the main page's window handle as string is good practice for future re-usable purposes
+        String mainHandle = driver.getWindowHandle();
+
+    System.out.println("mainHandle = " + mainHandle);
+
+    //4. Assert: Title is “Windows”
+    String expectedTitle = "Windows";
+    String actualTitle = driver.getTitle();
+
+    Assert.assertEquals(actualTitle, expectedTitle);
+
+    System.out.println("Title before click: " + actualTitle);
+
+    //5. Click to: “Click Here” link
+    WebElement clickHereLink = driver.findElement(By.linkText("Click Here"));
+
+    clickHereLink.click();
+
+    actualTitle = driver.getTitle();
+
+    System.out.println("Title after click: " + actualTitle);
+
+    //6. Switch to new Window.
+    //7. Assert: Title is “New Window”
 
 
 }
 
 
-//4. Assert: Title is “Windows”
-//5. Click to: “Click Here” link
-//6. Switch to new Window.
-//7. Assert: Title is “New Window”
+}
+
+
+
