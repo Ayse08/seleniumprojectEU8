9071e7f664083cf58972943c6288f8bb225e1555
Ayse <aysenur_aydin@hotmail.fr>
2022-06-02 15:14:19 +0200

    day9 Java Faker Practice

--- src/test/java/com/cydeo/utilities/Driver.java
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -1,6 +1,11 @@
 package com.cydeo.utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class Driver {
 
@@ -23,6 +28,27 @@ public class Driver {
 
         if (driver ==  null){
 
+            /*
+            We read browserType from configuration.properties.
+            This way, we can control which browser is opened from outside our code, from configuration.properties
+             */
+           String browserType = ConfigurationReader.getProperty("browser");
+
+           switch (browserType){
+               case "chrome":
+                   WebDriverManager.chromedriver().setup();
+                   driver = new ChromeDriver();
+                   driver.manage().window().maximize();
+                   driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                   break;
+
+               case "firefox":
+                   WebDriverManager.firefoxdriver().setup();
+                   driver = new FirefoxDriver();
+                   driver.manage().window().maximize();
+                   driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+           }
         }
         return driver;
 
