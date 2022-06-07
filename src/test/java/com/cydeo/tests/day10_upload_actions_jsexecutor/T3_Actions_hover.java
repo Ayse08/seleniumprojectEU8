ff711f5502c68628ee029986558cf7371edc607c
Ayse <aysenur_aydin@hotmail.fr>
2022-06-07 11:49:03 +0200

    day10 T3 Actions Hover

--- src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T3_Actions_hover.java
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T3_Actions_hover.java
@@ -1,6 +1,5 @@
 package com.cydeo.tests.day10_upload_actions_jsexecutor;
 
-import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -8,13 +7,13 @@ import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class T3_Actions_Hover {
+public class T3_Actions_hover {
 
     @Test
-    public void t3_actions_hover(){
+    public void hovering_test(){
 
-        //TC #3: Hover Test
-        //1. Go to https://practice.cydeo.com/hovers
+//TC #3: Hover Test
+//1. Go to https://practice.cydeo.com/hovers
         Driver.getDriver().get("https://practice.cydeo.com/hovers");
 
         //Locate all images under here:
@@ -22,38 +21,35 @@ public class T3_Actions_Hover {
         WebElement img2 = Driver.getDriver().findElement(By.xpath("(//img)[2]"));
         WebElement img3 = Driver.getDriver().findElement(By.xpath("(//img)[3]"));
 
-        //Locate all "user" texts under here:
+        //Locate all the "user" texts under here:
         WebElement user1 = Driver.getDriver().findElement(By.xpath("//h5[text()='name: user1']"));
         WebElement user2 = Driver.getDriver().findElement(By.xpath("//h5[text()='name: user2']"));
         WebElement user3 = Driver.getDriver().findElement(By.xpath("//h5[text()='name: user3']"));
 
         Actions actions = new Actions(Driver.getDriver());
 
-        //2. Hover over to first image
-        BrowserUtils.sleep(2);
+
+//2. Hover over to first image
         actions.moveToElement(img1).perform();
 
-        //3. Assert:
-        //a. “name: user1” is displayed
+//3. Assert:
+//a. “name: user1” is displayed
         Assert.assertTrue(user1.isDisplayed());
 
-
-        //4. Hover over to second image
-        BrowserUtils.sleep(2);
+//4. Hover over to second image
         actions.moveToElement(img2).perform();
 
-        //5. Assert:
-        //a. “name: user2” is displayed
+//5. Assert:
+//a. “name: user2” is displayed
         Assert.assertTrue(user2.isDisplayed());
 
-        //6. Hover over to third image
-        BrowserUtils.sleep(2);
+//6. Hover over to third image
         actions.moveToElement(img3).perform();
 
-        //7. Confirm:
-        //a. “name: user3” is displayed
+//7. Confirm:
+//a. “name: user3” is displayed
         Assert.assertTrue(user3.isDisplayed());
 
-
     }
+
 }
