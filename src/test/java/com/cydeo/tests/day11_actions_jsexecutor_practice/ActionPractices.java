4f15b8188fefa6495f7062a95e3e8d4732a34db8
Ayse <aysenur_aydin@hotmail.fr>
2022-06-07 16:18:57 +0200

    day11 ActionsPractices

--- src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/ActionPractices.java
+++ src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/ActionPractices.java
@@ -1,6 +1,10 @@
 package com.cydeo.tests.day11_actions_jsexecutor_practice;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 public class ActionPractices {
@@ -14,12 +18,27 @@ public class ActionPractices {
         Driver.getDriver().get("https://practice.cydeo.com/");
 
         //3- Scroll down to “Powered by CYDEO”
+        // Create object of Actions and pass our "driver" instance
+        Actions actions = new Actions(Driver.getDriver());
+
+        //Locating cydeo link to be able to pass in the actions method
+        WebElement cydeoLink = Driver.getDriver().findElement(By.linkText("CYDEO"));
+
         //4- Scroll using Actions class “moveTo(element)” method
-        //
-        //
+        actions.moveToElement(cydeoLink).pause(4000).perform();
+
         //TC #5: Scroll practice 2
         //1- Continue from where the Task 4 is left in the same test.
         //2- Scroll back up to “Home” link using PageUP button
+        actions.sendKeys(Keys.PAGE_UP, Keys.PAGE_UP).perform();
+
+        Driver.getDriver().quit(); // it completely terminates the driver instance
+        Driver.getDriver().close(); //
+
+    }
+
+    @Test
+    public void test2(){
 
     }
 
