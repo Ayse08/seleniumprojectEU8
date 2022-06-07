3a7b35ee1fc77b193dabe09e23784e8722a030e4
Ayse <aysenur_aydin@hotmail.fr>
2022-06-07 23:31:22 +0200

    day11 T1 Circle_drag_n_drop

--- src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/T7_Scroll_JSExecutor.java
+++ src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/T7_Scroll_JSExecutor.java
@@ -1,5 +1,9 @@
 package com.cydeo.tests.day11_actions_jsexecutor_practice;
 
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class T7_Scroll_JSExecutor {
@@ -9,8 +13,27 @@ public class T7_Scroll_JSExecutor {
         //TC #7: Scroll using JavascriptExecutor
         //1- Open a chrome browser
         //2- Go to: https://practice.cydeo.com/large
+        Driver.getDriver().get("https://practice.cydeo.com/large");
+
+        WebElement cydeoLink = Driver.getDriver().findElement(By.linkText("CYDEO"));
+        WebElement homeLink = Driver.getDriver().findElement(By.linkText("Home"));
+
+       // Down-casting our driver type to JavascriptExecutor, so we are able to use the methods coming from
+        // that interface
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+
         //3- Scroll down to “Cydeo” link
+        js.executeScript("arguments[0].scrollIntoView(true)", cydeoLink);
+        //scroll until you see it
+
         //4- Scroll up to “Home” link
+        js.executeScript("arguments[1].scrollIntoView(true)", cydeoLink, homeLink);
+
+        //objects
+        //cydeoLink 0
+        //homeLink 1
+        //someOtherLink 2
+
         //5- Use below provided JS method only
         //
         //JavaScript method to use : arguments[0].scrollIntoView(true)
